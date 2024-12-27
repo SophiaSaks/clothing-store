b188ce769ec5442b997914e8bffda16ed9c50865
sophia.saks@live.se <sophiasaks@MacBook-Pro-som-tillhor-Sophia.local>
2024-12-27 22:23:04 +0100

    created react app

---
+++ clothing-store-frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer () {
+return(
+    <div className='footer'>
+        <div className='madeBy'>
+        <span>
+        Made by Sophia Saks 2024/2025. All pictures are from Unsplash.com
+        </span>
+        </div>
+
+    </div>
+)
+}
+
+export default Footer
