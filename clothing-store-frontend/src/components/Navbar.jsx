e3c3892d544c5706aec042d8a59a3e7a7fedc312
sophia.saks@live.se <sophiasaks@MacBook-Pro-som-tillhor-Sophia.local>
2024-12-30 21:24:54 +0100

    adding icons

--- clothing-store-frontend/src/components/Navbar.jsx
+++ clothing-store-frontend/src/components/Navbar.jsx
@@ -1,35 +1,46 @@
 import './Navbar.css'
 import Logo from '../assets/_/logo.png'
 import '../zindexes.css'
+import searchIcon from '../assets/_/searchicon.svg'
+import shoppingBasket from '../assets/_/shoppingBasket.svg'
+import women from '../assets/_/female.svg'
+import men from '../assets/_/men.svg'
+import unisex from '../assets/_/Union.svg'
 
-function Navbar () {
-return(
-    <div className='navbar'>
+function Navbar() {
+    return (
+        <div className='navbar'>
             <ul>
                 <li>
+                    <img className="genderLogo" src={women}></img>
                     Women
                 </li>
                 <li>
+                    <img className="genderLogo" src={men}></img>
                     Men
                 </li>
                 <li>
+                    <img className="genderLogo" src={unisex}></img>
                     Unisex
                 </li>
             </ul>
-        <div className="logo">
-            <img className="logoImg" src={Logo} alt="logo"></img>
-        </div>
-        <form className="searchBar">
-            <label>Search</label>
-            <input type="text" placeholder="search here..."></input>
-            <button>Search</button>
-        </form>
-        <div className="cart">
-            <span>Cart</span>
-        </div>
+            <div className="logo">
+                <img className="logoImg" src={Logo} alt="logo"></img>
+            </div>
+            <form className="searchBar">
+                <input type="text" placeholder="Search here..."></input>
+                <button className="searchButton">
+                    <img src={searchIcon}></img>
+                </button>
+            </form>
+            <div className="cart">
+                <img src={shoppingBasket}></img>
+                <span>Cart</span>
+                <span className="cartNumber">12</span>
+            </div>
 
-    </div>
-)
+        </div>
+    )
 }
 
 export default Navbar
