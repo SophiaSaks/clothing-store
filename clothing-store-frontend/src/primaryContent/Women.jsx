86abfe5145f8936b1ec00b0340070cfdb33f9def
Sophia S <sophia.saks@live.se>
2025-01-26 02:29:05 +0100

    WIP

--- clothing-store-frontend/src/primaryContent/Women.jsx
+++ clothing-store-frontend/src/primaryContent/Women.jsx
@@ -4,10 +4,15 @@ import statueWoman from '../assets/_/_/_/statueWoman.png'
 
 
 const products = [
-    { src: statueWoman, title: "T-shirt", price: "100 SEK", caption: "Female clothing", key: "0", className:"product1"},
-    { src: statueWoman, title: "Dress", price: "200 SEK", caption: "Female clothing", key: "1" , className:"product2"},
-    { src: statueWoman, title: "T-shirt", price: "400 SEK", caption: "Female clothing", key: "2", className:"product3" },
-    { src: statueWoman, title: "Scarf", price: "100 SEK", caption: "Female clothing", key: "3", className:"product4"}
+    { src: statueWoman, title: "Tshirt", price: "100 SEK", caption: "Female clothing", key: "0", className:"product"},
+    { src: statueWoman, title: "Dress", price: "200 SEK", caption: "Female clothing", key: "1" , className:"product"},
+    { src: statueWoman, title: "Tshirt", price: "400 SEK", caption: "Female clothing", key: "2", className:"product" },
+    { src: statueWoman, title: "dress", price: "100 SEK", caption: "Female clothing", key: "3", className:"product"},
+    { src: statueWoman, title: "Tshirt", price: "100 SEK", caption: "Female clothing", key: "0", className:"product"},
+    { src: statueWoman, title: "Dress", price: "200 SEK", caption: "Female clothing", key: "1" , className:"product"},
+    { src: statueWoman, title: "Tshirt", price: "400 SEK", caption: "Female clothing", key: "2", className:"product" },
+    { src: statueWoman, title: "dress", price: "100 SEK", caption: "Female clothing", key: "3", className:"product"}
+
 ]
 
 function Women() {
@@ -18,19 +23,10 @@ function Women() {
                     {products.map((products, index) => (
                         <div className={products.className} key={index}>
                             <img src={products.src} alt={products.caption} />
+                            <div className="products-info"> 
                             <span className={products.className}>{products.title}</span>
                             <p>{products.price}</p>
-                        </div>
-                    ))}
-                </li>
-            </ul>
-            <ul className="WomenUl">
-                <li className="WomenLi">
-                    {products.map((products, index) => (
-                        <div className={products.className} key={index}>
-                            <img src={products.src} alt={products.caption} />
-                            <span className={products.className}>{products.title}</span>
-                            <p>{products.price}</p>
+                            </div>
                         </div>
                     ))}
                 </li>
